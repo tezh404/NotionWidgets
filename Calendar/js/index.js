8c3690ab8d6e9c91f73c1ed5e0fb22ac5ff23580
halil tezel <tezelhalil@hotmail.com>
2023-08-17 11:29:11 +0300

    Fix: week day starts on monday.

--- Calendar/js/index.js
+++ Calendar/js/index.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
     prevText: '<i class="fa fa-fw fa-angle-left"></i>',
     nextText: '<i class="fa fa-fw fa-angle-right"></i>'
   });
+
+// Getter
+var firstDay = $( ".datepicker" ).datepicker( "option", "firstDay" );
+ 
+// Setter
+$( ".datepicker" ).datepicker( "option", "firstDay", 1 );
+
+
 });
